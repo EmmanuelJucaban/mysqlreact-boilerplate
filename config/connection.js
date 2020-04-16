4d9e3ba739d2083abfe1bb02b8985e6de93e8747
emmanueljucaban <emmanueljucaban@gmail.com>
2020-04-15 19:20:04 -0700

    Push to fix heroku

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,9 @@
 const mysql = require('mysql');
 let connection;
+
+
 if(process.env.JAWSDB_URL) {
-    connection = connection.createConnection(process.env.JAWSDB_URL);
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
         host: 'localhost',
